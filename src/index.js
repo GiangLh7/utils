5f0bd75057c44203d1f368dd7db16f3bad91f3e4
Unknown <hoang_giang_bk@yahoo.com>
2017-09-24 23:01:24 +0700

    update package.json
    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import Request from './request/request';
+import TokenIssuer from './authentication/tokenissuer';
+import Authorization from './authentication/authorization';
 
 exports.Request = Request;
+exports.Authorization = Authorization;
+exports.TokenIssuer = TokenIssuer;
